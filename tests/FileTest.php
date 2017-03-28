2ca685b1d2ca922eb503da423cba2084c904677c
Anton Smirnov <sandfox@sandfox.me>
2017-03-29 00:16:53 +0300

    test file functions

---
+++ tests/FileTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use SandFoxMe\Bencode\Bencode;
+
+class FileTest extends TestCase
+{
+    public function testFile()
+    {
+        $file       = '/tmp/bencode_test_dump.torrent';
+        $value      = [1, 2, 3, 4, 5];
+        $encoded    = Bencode::encode($value);
+
+        Bencode::dump($file, $value);
+
+        $onDisk = file_get_contents($file);
+
+        $this->assertEquals($encoded, $onDisk);
+
+        $loaded = Bencode::load($file);
+
+        $this->assertEquals($loaded, $value);
+
+        unlink($file);
+    }
+}
