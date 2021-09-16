65aa27f4a87dc14998d16fb8bc018d8a814d415d
Anton Smirnov <sandfox@sandfox.me>
2021-09-17 01:25:14 +0300

    Backport DictType

---
+++ src/Types/IterableTypeTrait.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SandFox\Bencode\Types;
+
+trait IterableTypeTrait
+{
+    private \Traversable $traversable;
+
+    /**
+     * @param iterable $iterable Iterable to be wrapped
+     */
+    public function __construct(iterable $iterable)
+    {
+        if (\is_array($iterable)) {
+            $iterable = new \ArrayIterator($iterable);
+        }
+
+        $this->setIterator($iterable);
+    }
+
+    private function setIterator(\Traversable $traversable): void
+    {
+        $this->traversable = $traversable;
+    }
+
+    public function getIterator(): \Traversable
+    {
+        return $this->traversable;
+    }
+}
