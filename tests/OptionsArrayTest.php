36aa2db2bae2d821183ba015be70ab6c913f80a2
Anton Smirnov <sandfox@sandfox.me>
2022-12-14 01:25:46 +0200

    Test missing coverage

---
+++ tests/OptionsArrayTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Arokettu\Bencode\Tests;
+
+use Arokettu\Bencode\Bencode;
+use PHPUnit\Framework\TestCase;
+
+class OptionsArrayTest extends TestCase
+{
+    public function testDecoder(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('$options array must not be used');
+
+        Bencode::decode('de', ['any' => 'thing']);
+    }
+
+    public function testEncoder(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('$options array must not be used');
+
+        Bencode::encode([], ['any' => 'thing']);
+    }
+}
