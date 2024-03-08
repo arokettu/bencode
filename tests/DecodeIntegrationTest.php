4daa263f0eb46ff7cae5478476c62c27b64d75fb
Anton Smirnov <sandfox@sandfox.me>
2024-03-08 22:31:50 +0200

    Test various nestings

--- tests/DecodeIntegrationTest.php
+++ tests/DecodeIntegrationTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Arokettu\Bencode\Tests;
 
 use Arokettu\Bencode\Bencode;
+use Arokettu\Bencode\Bencode\Collection;
 use ArrayObject;
 use PHPUnit\Framework\TestCase;
 use stdClass;
@@ -56,5 +57,95 @@ class DecodeIntegrationTest extends TestCase
         self::assertEquals($value, $decoded);
         self::assertEquals(ArrayObject::class, $decoded[3]::class);
         self::assertEquals(stdClass::class, $decoded[4]::class);
+    }
+
+    // various nestings
+
+    public function testDictListDict(): void
+    {
+        $value = [
+            'a' => 'x',
+            'b' => 'y',
+            'c' => [
+                1,
+                2,
+                3 => [
+                    'a' => 'x',
+                    'b' => 'y',
+                    'c' => 'z',
+                ],
+                4,
+            ],
+            'd' => 'z',
+        ];
+
+        $encoded = Bencode::encode($value);
+        self::assertEquals($value, Bencode::decode($encoded, listType: Collection::ARRAY, dictType: Collection::ARRAY));
+    }
+
+    public function testListDictList(): void
+    {
+        $value = [
+            1,
+            2,
+            3 => [
+                'a' => 'x',
+                'b' => 'y',
+                'c' => [
+                    1,
+                    2,
+                    3,
+                ],
+                'd' => 'z',
+            ],
+            4,
+        ];
+
+        $encoded = Bencode::encode($value);
+        self::assertEquals($value, Bencode::decode($encoded, listType: Collection::ARRAY, dictType: Collection::ARRAY));
+    }
+
+    public function testListListList(): void
+    {
+        $value = [
+            1,
+            2,
+            3 => [
+                1,
+                2,
+                3 => [
+                    1,
+                    2,
+                    3,
+                ],
+                4,
+            ],
+            4,
+        ];
+
+        $encoded = Bencode::encode($value);
+        self::assertEquals($value, Bencode::decode($encoded, listType: Collection::ARRAY, dictType: Collection::ARRAY));
+    }
+
+    public function testDictDictDict(): void
+    {
+        $value = [
+            'a' => 'x',
+            'b' => 'y',
+            'c' => [
+                'a' => 'x',
+                'b' => 'y',
+                'c' => [
+                    'a' => 'x',
+                    'b' => 'y',
+                    'c' => 'z',
+                ],
+                'd' => 'z',
+            ],
+            'd' => 'z',
+        ];
+
+        $encoded = Bencode::encode($value);
+        self::assertEquals($value, Bencode::decode($encoded, listType: Collection::ARRAY, dictType: Collection::ARRAY));
     }
 }
